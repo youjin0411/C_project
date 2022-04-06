64dd9b9bd635b4ab02dee2b52e02c9d751607486
김유진 <w2117@e-mirim.hs.kr>
2022-04-07 00:07:47 +0900

    cospro2

---
+++ myhalf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int arr[], int arr_len)
+{
+    int answer = 0;
+    for (int i = 0; i < arr_len; i++)
+        for (int j = 0; j < arr_len; j++)
+            if (arr[i] % 2 == 0 && arr[i] / 2 == arr[j])
+            {
+                answer++;
+                break;
+            }
+    return answer;
+}
+
+int main()
+{
+    int arr[] = {4, 8, 3, 6, 3};
+    int arr_len = 5;
+    int ret = solution(arr, arr_len);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
